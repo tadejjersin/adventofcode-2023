84928f7207ae3fbda13475886cb13441347832ac
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2023-12-03 09:59:57 +0100

    cleaner day03

--- scripts/day03.py
+++ scripts/day03.py
@@ -12,30 +12,6 @@ def neighbours(i, start, end):
     return lst
 
 s = 0
-
-for i in range(len(data)):
-    j = 0
-    while j < len(data[i]):
-        if data[i][j].isdigit():
-            start = j
-            num = ""
-            while j < len(data[i]) and data[i][j].isdigit():
-                num += data[i][j]
-                j += 1
-            end = j
-            num = int(num)
-            for k, l in neighbours(i, start, end):
-                if 0 <= k < len(data) and 0 <= l < len(data[0]) and data[k][l] != ".":
-                    s += num
-                    break
-        else:
-            j += 1
-
-print(s)
-
-#part 2
-
-s2 = 0
 d = dict()
 
 for i in range(len(data)):
@@ -49,14 +25,24 @@ for i in range(len(data)):
                 j += 1
             end = j
             num = int(num)
+            flag = True
             for k, l in neighbours(i, start, end):
                 if 0 <= k < len(data) and 0 <= l < len(data[0]) and data[k][l] != ".":
+                    if flag:
+                        s += num
+                        flag = False
                     if (k, l) in d:
                         d[(k, l)].append(num)
                     else:
                         d[(k, l)] = [num]
         else:
             j += 1
+
+print(s)
+
+#part 2
+
+s2 = 0
 
 for key in d.keys():
     if len(d[key]) == 2:
